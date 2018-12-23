685b615cbad36fe22b609dc44d06651e8c2ab93d
bojan9 <9spasovski@gmail.com>
2018-12-23 19:17:50 +0100

    eve go, no ne e celosno napraven( ne smee da se pisuva prvin znak i od koga ke se klikne ednakvo, pa napise nov broj rezultatot treba da se izbrise)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-var currentValue = $("#currentValue");
+//var currentValue = $("#currentValue");
+//
+//$(".btn").click(function(event) {
+// var clicked = this.innerHTML
+//  currentValue.html(clicked)
+//})
 
-$(".btn").click(function(event) {
- var clicked = this.innerHTML
-  currentValue.html(clicked)
-})
+function number(x){
+document.getElementById("display").value+=x;
+}
+
+function equal(){
+		var x = document.getElementById("display").value;
+		document.getElementById("display").value = eval(x);
+}
